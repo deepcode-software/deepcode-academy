16b3d3a1e6db22bc1b56a11492203d0c27dbbb87
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-10-19 23:08:37 +0500

    feat: refactor Home component structure to use section and container for improved semantics and styling

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-
 export default function Home() {
   return (
-    <div className="home">
-      <h1>DeepCode Academy</h1>
-      <h2>Online Academy Platform</h2>
-      <div className="search">
-        <input
-          type="search"
-          name="search"
-          placeholder="What Do You Need To Learn?"
-        />
+    <section className="home">
+      <div className="container">
+        <h1>DeepCode Academy</h1>
+        <h2>Online Academy Platform</h2>
+        <div className="search">
+          <input
+            type="search"
+            name="search"
+            placeholder="What Do You Need To Learn?"
+          />
         <button>Search</button>
       </div>
-    </div>
+      </div>
+    </section>
   );
 }
