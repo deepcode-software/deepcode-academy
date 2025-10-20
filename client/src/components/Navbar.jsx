b8d5e0d12ad341f8e2b4a278da16c78e95f1cc40
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-10-20 21:00:16 +0500

    feat: refactor Navbar and Home components for improved structure and styling

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,40 +1,45 @@
-import React from "react";
-import "../styles/globals.css";
-import { Link } from "react-router-dom";
+  import "../styles/globals.css";
+  import { Link } from "react-router-dom";
 
-export default function Navbar() {
-  return (
-    <header>
-      <div className="top-nav">
-        <div className="contact">
-          <p>+998 93 085 0955</p>
-          <p>deepcodeacademy@gmail.com</p>
+  export default function Navbar() {
+    return (
+      <header>
+        <div className="container">
+          <div className="header_content">
+            <div className="top-nav">
+              <div className="contact">
+                <p>+998 93 085 0955</p>
+                <p>deepcodeacademy@gmail.com</p>
+              </div>
+              <div className="address">
+                <p>Navoiy Uzbekistan</p>
+              </div>
+            </div>
+            <nav>
+              <div className="container">
+                <div className="logo">
+                  <h1>
+                    <span>DEEP</span>CODE
+                  </h1>
+                </div>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About</Link>
+                  </li>
+                  <li>
+                    <Link to="/courses">Courses</Link>
+                  </li>
+                  <li>
+                    <Link to="/blog">Blog</Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
         </div>
-        <div className="address">
-          <p>Navoiy Uzbekistan</p>
-        </div>
-      </div>
-      <nav>
-        <div className="logo">
-          <h1>
-            <span>DEEP</span>CODE
-          </h1>
-        </div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/courses">Courses</Link>
-          </li>
-          <li>
-            <Link to="/blog">Blog</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+      </header>
+    );
+  }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,9 @@
 export default function Home() {
   return (
-    <section className="home">
+    <div>
       <div className="container">
-        <h1>DeepCode Academy</h1>
-        <h2>Online Academy Platform</h2>
-        <div className="search">
-          <input
-            type="search"
-            name="search"
-            placeholder="What Do You Need To Learn?"
-          />
-        <button>Search</button>
+
       </div>
-      </div>
-    </section>
+    </div>
   );
 }
